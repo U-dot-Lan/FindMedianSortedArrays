a5a7f4a609ec441252418a6a235ce22ade2797e9
Ulanbek Bolotbekov <drreal@mail.ru>
2021-12-29 20:59:55 +0300

    Добавьте файлы проекта.

---
+++ FMSA.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FindMedianSortedArrays
+{
+    public static class FMSA
+    {
+        public static double FindMedian(int[] nums1, int[] nums2)
+        {
+                int l1 = nums1.Length;
+                int l2 = nums2.Length;
+                int length = l1 + l2;
+                var resultIndex = length / 2;
+                List<int> list = new List<int>(nums1);
+                list.AddRange(nums2);
+                list.Sort();
+                if (length % 2 == 0)
+                {
+                    return (list[resultIndex - 1] + list[resultIndex]) / 2.0;
+                }
+                else
+                {
+                    return list[resultIndex];
+                }
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FindMedianSortedArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums1 = new int[] { 1, 3 };
+            int[] nums2 = new int[] { 2 };
+            double result = FMSA.FindMedian(nums1, nums2);
+            Console.WriteLine(result);
+
+            int[] nums3 = new int[] { 1, 2 };
+            int[] nums4 = new int[] { 3, 4 };
+            double result1 = FMSA.FindMedian(nums3, nums4);
+            Console.WriteLine(result1);
+
+        }
+    }
+}
